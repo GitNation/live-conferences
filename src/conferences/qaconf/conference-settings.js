28d1dc2877bc51a510720513ee01665460942fdd
Oleg Proskurin <regx@usul.su>
2020-06-26 22:48:20 +0300

    Copy QA conf

---
+++ src/conferences/qaconf/conference-settings.js
@@ -0,0 +1,91 @@
+const conferenceTitle = 'QA_Global_Summit';
+const eventYear = 'Y2020';
+
+const tagColors = {
+  "AI/ML": {
+    tagBG: '#dd4b39',
+    color: '#fbff00',
+  },
+  "UX/UI": {
+    tagBG: '#5e8de3',
+    color: '#FFFFFF',
+  },
+  NodeJS: {
+    tagBG: '#8cc84b',
+    color: '#fff',
+  },
+  WebGL: {
+    tagBG: '#ff7302',
+    color: '#ffffff',
+  },
+  Ecosystem: {
+    tagBG: '#fff40d',
+    color: '#ae4f01',
+  },
+  Language: {
+    tagBG: '#fff40d',
+    color: '#ae4f01',
+  },
+  GraphQL: {
+    tagBG: '#f200fa',
+    color: '#fff',
+  },
+  GQL: {
+    tagBG: '#198bb0',
+    color: '#FFFFFF',
+  },
+  VueJS: {
+    tagBG: '#3fb984',
+    color: '#fff',
+  },
+  Performance: {
+    tagBG: '#00ed24',
+    color: '#00410a',
+  },
+  TypeScript: {
+    tagBG: '#007acc',
+    color: '#fff',
+  },
+  Angular: {
+    tagBG: '#dd0031',
+    color: '#FFFFFF',
+  },
+  AlpineJS: {
+    tagBG: '#8ac1ce',
+    color: '#fff',
+  },
+  React: {
+    tagBG: '#61dbfb',
+    color: '#004d61',
+  },
+  I18N: {
+    tagBG: '#866bba',
+    color: '#b9efff',
+  },
+  Career: {
+    tagBG: '#9c00ff',
+    color: '#fff',
+  },
+  Testing: {
+    tagBG: '#ffaa45',
+    color: '#fff',
+  },
+  default: {
+    tagBG: 'black',
+    color: 'white',
+  }
+};
+
+const speakerAvatar = {
+  dimensions: {
+    avatarWidth: 500,
+    avatarHeight: 500,
+  },
+};
+
+module.exports = {
+  tagColors,
+  speakerAvatar,
+  conferenceTitle,
+  eventYear,
+};
