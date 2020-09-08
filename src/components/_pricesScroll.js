815c6765b426e75d19b684f64c666817a8f242a9
Denis Radin <dradin@evolutiongaming.com>
2020-09-08 17:46:32 +0200

    Google Tag manager init

--- src/components/_pricesScroll.js
+++ src/components/_pricesScroll.js
@@ -1,37 +1,39 @@
 export default function pricesScroll() {
   var elem = document.getElementById('js-prices-scroll');
   var glow = document.getElementById('js-prices-glow');
-  var elemChildren = elem.children;
+  if (elem) {
+    var elemChildren = elem.children;
 
-  var elemChildrenWidth = elemChildren[1].offsetWidth;
-  var elemWidth = elem.offsetWidth;
-  var elemChildElementCount = elem.childElementCount - 1;
-  var elemTotalWidth = elemChildElementCount * elemChildrenWidth;
-  var hasScrollbars = elem.scrollHeight !== elem.offsetHeight;
+    var elemChildrenWidth = elemChildren[1].offsetWidth;
+    var elemWidth = elem.offsetWidth;
+    var elemChildElementCount = elem.childElementCount - 1;
+    var elemTotalWidth = elemChildElementCount * elemChildrenWidth;
+    var hasScrollbars = elem.scrollHeight !== elem.offsetHeight;
 
-  function update() {
-    var elemScrollValue = elem.scrollLeft;
-    var glowPosRight = elemScrollValue;
-    glow.style.right = -glowPosRight + 'px';
+    function update() {
+      var elemScrollValue = elem.scrollLeft;
+      var glowPosRight = elemScrollValue;
+      glow.style.right = -glowPosRight + 'px';
 
-    if (glowPosRight + elemWidth >= elemTotalWidth) {
-      glow.style.opacity = '0';
-    } else {
-      glow.style.opacity = '1';
+      if (glowPosRight + elemWidth >= elemTotalWidth) {
+        glow.style.opacity = '0';
+      } else {
+        glow.style.opacity = '1';
+      }
     }
   }
 
   window.addEventListener('resize', () => {
-  	if (elem.scrollHeight === elem.offsetHeight) {
-  		glow.style.visibility = 'hidden';
-  	} else {
-  		glow.style.visibility = 'visible';
-  	}
+    if (elem.scrollHeight === elem.offsetHeight) {
+      glow.style.visibility = 'hidden';
+    } else {
+      glow.style.visibility = 'visible';
+    }
 
-  	// console.log(elem.scrollHeight !== elem.offsetHeight);
+    // console.log(elem.scrollHeight !== elem.offsetHeight);
   });
 
   elem.addEventListener('scroll', () => {
-  	update();
+    update();
   });
 }
