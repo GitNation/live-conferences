69ded8b2f4c4071aa654c45dce2dc8cef25f3fba
Oleg Proskurin <regx@usul.su>
2021-02-12 02:55:34 +0300

    Resolve LIVE-CONFERENCES-3

--- src/components/_pricesScroll.js
+++ src/components/_pricesScroll.js
@@ -1,32 +1,32 @@
 export default function pricesScroll() {
   var elem = document.getElementById('js-prices-scroll');
   var glow = document.getElementById('js-prices-glow');
-  
-  if (elem) {
-    var elemChildren = elem.children;
 
-    var elemChildrenWidth = elemChildren[1].offsetWidth;
-    var elemWidth = elem.offsetWidth;
-    var elemChildElementCount = elem.childElementCount - 1;
-    var elemTotalWidth = elemChildElementCount * elemChildrenWidth;
-    var hasScrollbars = elem.scrollHeight !== elem.offsetHeight;
-
-    function update() {
-      var elemScrollValue = elem.scrollLeft;
-      var glowPosRight = elemScrollValue;
-      glow.style.right = -glowPosRight + 'px';
-
-      if (glowPosRight + elemWidth >= elemTotalWidth) {
-        glow.style.opacity = '0';
-      } else {
-        glow.style.opacity = '1';
-      }
-    }
-
-    elem.addEventListener('scroll', () => {
-      update();
-    });
+  if (!elem) {
+    return;
   }
+  var elemChildren = elem.children;
+
+  var elemChildrenWidth = elemChildren[1].offsetWidth;
+  var elemWidth = elem.offsetWidth;
+  var elemChildElementCount = elem.childElementCount - 1;
+  var elemTotalWidth = elemChildElementCount * elemChildrenWidth;
+
+  function update() {
+    var elemScrollValue = elem.scrollLeft;
+    var glowPosRight = elemScrollValue;
+    glow.style.right = -glowPosRight + 'px';
+
+    if (glowPosRight + elemWidth >= elemTotalWidth) {
+      glow.style.opacity = '0';
+    } else {
+      glow.style.opacity = '1';
+    }
+  }
+
+  elem.addEventListener('scroll', () => {
+    update();
+  });
 
   window.addEventListener('resize', () => {
     if (elem.scrollHeight === elem.offsetHeight) {
@@ -35,6 +35,5 @@ export default function pricesScroll() {
       glow.style.visibility = 'visible';
     }
 
-    // console.log(elem.scrollHeight !== elem.offsetHeight);
   });
 }
