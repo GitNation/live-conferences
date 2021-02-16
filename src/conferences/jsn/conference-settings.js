2976ee4d4d25acc122bff2636cfde2a495470452
Oleg Shelest <lateexairu@gmail.com>
2021-02-16 17:16:20 +0200

    update jsn tags

--- src/conferences/jsn/conference-settings.js
+++ src/conferences/jsn/conference-settings.js
@@ -2,48 +2,112 @@ const conferenceTitle = 'Live_JSNation';
 const eventYear = 'Y2021';
 
 const tagColors = {
-  NodeJS: {
-    tagBG: '#7AB464',
-    color: '#fff',
-  },
-  'NestJS': {
-    tagBG: '#ea2845',
-    color: '#fff',
-  },
-  WebGL: {
-    tagBG: '#ff7302',
-    color: '#ffffff',
-  },
-  Ecosystem: {
-    tagBG: '#fff40d',
-    color: '#ae4f01',
-  },
-  Language: {
-    tagBG: '#fff40d',
-    color: '#ae4f01',
-  },
-  GraphQL: {
-    tagBG: '#f200fa',
-    color: '#400042',
-  },
-  PWA: {
-    tagBG: '#d400fa',
-    color: '#fff',
-  },
-  VueJS: {
-    tagBG: '#4EBA87',
-    color: '#fff',
-  },
-  Performance: {
-    tagBG: '#00ed24',
-    color: '#00410a',
-  },
-  TypeScript: {
-    tagBG: '#61DAFB',
+  "VueJS/Vite": {
+    tagBG: '#cdff08',
     color: '#030303',
   },
+  VueJS: {
+    tagBG: '#cdff08',
+    color: '#030303',
+  },
+  React: {
+    tagBG: '#e9bf98',
+    color: '#030303',
+  },
+  "Creative Coding": {
+    tagBG: '#974ca6',
+    color: '#fff',
+  },
+  "Career Advice": {
+    tagBG: '#633d30',
+    color: '#fff',
+  },
+  Snowpack: {
+    tagBG: '#3db366',
+    color: '#fff',
+  },
+  TC39: {
+    tagBG: '#9fa8e1',
+    color: '#fff',
+  },
+  TensorFlow: {
+    tagBG: '#f03c6d',
+    color: '#fff',
+  },
+  Yarn: {
+    tagBG: '#7f8861',
+    color: '#fff',
+  },
+  Jest: {
+    tagBG: '#2b6abe',
+    color: '#fff',
+  },
+  VSCode: {
+    tagBG: '#bf11af',
+    color: '#fff',
+  },
+  Electron: {
+    tagBG: '#a5d1b3',
+    color: '#030303',
+  },
+  "Next.js": {
+    tagBG: '#255026',
+    color: '#fff',
+  },
+  Deno: {
+    tagBG: '#ec7f6a',
+    color: '#fff',
+  },
+  Stimulus: {
+    tagBG: '#f2c029',
+    color: '#fff',
+  },
+  Stimulus: {
+    tagBG: '#20f53d',
+    color: '#030303',
+  },
+  LitElement: {
+    tagBG: '#fe74fe',
+    color: '#fff',
+  },
+  "Edge Handlers": {
+    tagBG: '#573bed',
+    color: '#fff',
+  },
+  WebAssembly: {
+    tagBG: '#8a1b07',
+    color: '#fff',
+  },
+  VanillaJS: {
+    tagBG: '#3fc6f8',
+    color: '#fff',
+  },
+  Fastify: {
+    tagBG: '#f9380a',
+    color: '#fff',
+  },
+  Testing: {
+    tagBG: '#fbacf6',
+    color: '#fff',
+  },
+  Security: {
+    tagBG: '#37fcea',
+    color: '#030303',
+  },
+  NodeJS: {
+    tagBG: '#97f989',
+    color: '#030303',
+  },
+  TypeScript: {
+    tagBG: '#fe8f06',
+    color: '#fff',
+  },
   Svelte: {
-    tagBG: '#ff5200',
+    tagBG: '#ff3c00',
+    color: '#fff',
+  },
+  NestJS: {
+    tagBG: '#e1234e',
     color: '#fff',
   },
   default: {
