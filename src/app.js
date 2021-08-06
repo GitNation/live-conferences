c192602c39516a92599feff794aab5efa2b6ca59
Robert Haritonov <1260461+operatino@users.noreply.github.com>
2021-08-06 18:00:43 +0100

    update sentry, add core web vital measurment to GA

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import './components/noticePanel';
 // import './components/_timeTrack';
 import noTouch from './components/noTouch';
 import ticketNotFound from './components/ticketNotFound';
+import { getCLS, getFID, getLCP } from 'web-vitals';
 
 Sentry.init({
   dsn: 'https://60b10886207d461a8b333f66e3d86ebf@o513607.ingest.sentry.io/5615857',
@@ -111,3 +112,32 @@ countdown();
 startHashLinksTracking();
 
 highlightContent({ contentTypeMap });
+
+function sendToGoogleAnalytics({ name, delta, id }) {
+  // Assumes the global `gtag()` function exists, see:
+  // https://developers.google.com/analytics/devguides/collection/gtagjs
+  gtag('event', name, {
+    event_category: 'Web Vitals',
+    // The `id` value will be unique to the current page load. When sending
+    // multiple values from the same page (e.g. for CLS), Google Analytics can
+    // compute a total by grouping on this ID (note: requires `eventLabel` to
+    // be a dimension in your report).
+    event_label: id,
+    // Google Analytics metrics must be integers, so the value is rounded.
+    // For CLS the value is first multiplied by 1000 for greater precision
+    // (note: increase the multiplier for greater precision if needed).
+    value: Math.round(name === 'CLS' ? delta * 1000 : delta),
+    // Use a non-interaction event to avoid affecting bounce rate.
+    non_interaction: true,
+
+    // OPTIONAL: any additional params or debug info here.
+    // See: https://web.dev/debug-web-vitals-in-the-field/
+    // metric_rating: 'good' | 'ni' | 'poor',
+    // debug_info: '...',
+    // ...
+  });
+}
+
+getCLS(sendToGoogleAnalytics);
+getFID(sendToGoogleAnalytics);
+getLCP(sendToGoogleAnalytics);
