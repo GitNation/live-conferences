2d184018e5505983c7a5fd470f6b29f4f9b289b6
Oleg Shelest <lateexairu@gmail.com>
2021-08-27 01:16:47 +0300

    add wgds conference

---
+++ src/conferences/wgds/conference-settings.js
@@ -0,0 +1,106 @@
+const conferenceTitle = 'Web_Gamedev_Summit';
+const eventYear = 'Y2021';
+
+const tagColors = {
+  Packaging: {
+    tagBG: '#dd4b39',
+    color: '#fbff00',
+  },
+  "CI/CD": {
+    tagBG: '#5e8de3',
+    color: '#FFFFFF',
+  },
+  "Serverless Deployment, AWS": {
+    tagBG: '#8cc84b',
+    color: '#fff',
+  },
+  Architecture: {
+    tagBG: '#ff7302',
+    color: '#ffffff',
+  },
+  AWS: {
+    tagBG: '#fff40d',
+    color: '#ae4f01',
+  },
+  DevOps: {
+    tagBG: '#fff40d',
+    color: '#ae4f01',
+  },
+  "CI/CD, AWS": {
+    tagBG: '#f200fa',
+    color: '#fff',
+  },
+  Bundler: {
+    tagBG: '#198bb0',
+    color: '#FFFFFF',
+  },
+  "Dev Pipelines": {
+    tagBG: '#3fb984',
+    color: '#fff',
+  },
+  "Web Hosting": {
+    tagBG: '#00ed24',
+    color: '#00410a',
+  },
+  Bundler: {
+    tagBG: '#007acc',
+    color: '#fff',
+  },
+  Performance: {
+    tagBG: '#dd0031',
+    color: '#FFFFFF',
+  },
+  "Feature Flagging": {
+    tagBG: '#8ac1ce',
+    color: '#fff',
+  },
+  Security: {
+    tagBG: '#61dbfb',
+    color: '#004d61',
+  },
+  Nx: {
+    tagBG: '#00a6ff',
+    color: '#fff',
+  },
+  Serverless: {
+    tagBG: '#9c00ff',
+    color: '#fff',
+  },
+  "Quality & Security": {
+    tagBG: '#ffaa45',
+    color: '#fff',
+  },
+  // "December 7": {
+  //   tagBG: '#00a6ff',
+  //   color: '#fff',
+  // },
+  // "December 8": {
+  //   tagBG: '#ff00d2',
+  //   color: '#fff',
+  // },
+  // "Lightning Talk": {
+  //   tagBG: '#9b4fe8',
+  //   color: '#fff',
+  // },
+  // "Workshops": {
+  //   tagBG: '#8e67ca',
+  //   color: '#fff',
+  // },
+  default: {
+    tagBG: 'black',
+    color: 'white',
+  }
+};
+const speakerAvatar = {
+  dimensions: {
+    avatarWidth: 500,
+    avatarHeight: 500,
+  },
+};
+
+module.exports = {
+  tagColors,
+  speakerAvatar,
+  conferenceTitle,
+  eventYear,
+};
