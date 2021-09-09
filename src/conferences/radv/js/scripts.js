3f57957c406e7413fbe7d0ea07b05fd3606d5692
Oleg Shelest <lateexairu@gmail.com>
2021-09-10 02:27:07 +0300

    fix radv schedule

--- src/conferences/radv/js/scripts.js
+++ src/conferences/radv/js/scripts.js
@@ -90,12 +90,12 @@ $(function() {
 
   /*---------------------------------------------------*/
   // Tabs
-  $('.js-tabs__tab').on('click', function(e) {
-    e.preventDefault();
-    $('.js-tabs__tab, .js-tabs__panel').removeClass('active');
-    $(this).add('#' + $(this).attr('id').replace(/\s*tab\s*/, 'panel')).addClass('active');
-    $(this).focus();
-  });
+  // $('.js-tabs__tab').on('click', function(e) {
+  //   e.preventDefault();
+  //   $('.js-tabs__tab, .js-tabs__panel').removeClass('active');
+  //   $(this).add('#' + $(this).attr('id').replace(/\s*tab\s*/, 'panel')).addClass('active');
+  //   $(this).focus();
+  // });
 
   /*---------------------------------------------------*/
   // Burger menu
