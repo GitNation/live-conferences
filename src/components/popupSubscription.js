3930fde8a6cf6029c3356a6367ba757a8e906157
Oleg Shelest <lateexairu@gmail.com>
2022-10-19 00:49:25 +0300

    update popupSubscription logic

--- src/components/popupSubscription.js
+++ src/components/popupSubscription.js
@@ -2,16 +2,31 @@ export default function popupSubscription() {
   const container = $('.js-subscription-container');
   const close = $('.js-subscription-close');
   const form = $('.js-subscription-form');
+  const showOnYOffset = document.body.scrollHeight / 5;
 
-  if (form.length) {
-    form[0].addEventListener('submit', () => {
-      container.addClass('done');
-    });
+  $(window).scroll(function() { 
+    var scrollY = $(document).scrollTop();
+    console.log('showOnYOffset ' + showOnYOffset);
 
-    for (const closeItem of close) {
-      closeItem.addEventListener('click', () => {
-        container.addClass('closed');
+    if (showOnYOffset < scrollY && !container.hasClass('visible') && localStorage.popupSubscriptionIsHidden !== 'true') {
+      container.addClass('visible');
+      console.log('show');
+    }
+  });
+
+  if (localStorage.popupSubscriptionIsHidden !== 'true') {
+    if (form.length) {
+      form[0].addEventListener('submit', () => {
+        container.addClass('done');
+        localStorage.setItem('popupSubscriptionIsHidden', true);
       });
+
+      for (const closeItem of close) {
+        closeItem.addEventListener('click', () => {
+          container.addClass('closed');
+          localStorage.setItem('popupSubscriptionIsHidden', true);
+        });
+      }
     }
   }
 }
