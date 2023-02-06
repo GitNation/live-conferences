0d231ab8853fc609d0eb4528037eb73ce0e28b83
Oleg Shelest <lateexairu@gmail.com>
2023-02-06 16:44:18 +0200

    remove speaker tags coloring

--- src/conferences/nodeconf/conference-settings.js
+++ src/conferences/nodeconf/conference-settings.js
@@ -2,62 +2,6 @@ const conferenceTitle = 'Node_Cyber_Summit';
 const eventYear = 'Y2023';
 
 const tagColors = {
-  "Feb 17": {
-    tagBG: '#bbff00',
-    color: '#000000',
-  },
-  "Feb 18": {
-    tagBG: '#000000',
-    color: '#bbff00',
-  },
-  Ecosystem: {
-    tagBG: '#fff40d',
-    color: '#ae4f01',
-  },
-  Language: {
-    tagBG: '#fff40d',
-    color: '#ae4f01',
-  },
-  GraphQL: {
-    tagBG: '#f200fa',
-    color: '#400042',
-  },
-  VueJS: {
-    tagBG: '#4EBA87',
-    color: '#fff',
-  },
-  Performance: {
-    tagBG: '#00ed24',
-    color: '#00410a',
-  },
-  TypeScript: {
-    tagBG: '#61DAFB',
-    color: '#030303',
-  },
-  'Summit Track': {
-    tagBG: '#a4ff00',
-    color: '#324e00',
-  },
-  'Base Camp Track': {
-    tagBG: '#696969',
-    color: '#e7e7e7',
-  },
-  'Lightning Talk': {
-    tagBG: '#2acadd',
-    color: '#003238',
-  },
-  'February 18': {
-    tagBG: '#414240',
-    color: '#fff',
-  },
-  'February 19': {
-    tagBG: '#b4b4b4',
-    color: '#020202',
-  },
-  'Workshop': {
-    tagBG: '#bbff00',
-    color: '#020202',
-  },
   default: {
     tagBG: 'black',
     color: 'white',
