2b76633715bb44fd2bcf80f8feaa5ede31ed94e0
lero62 <weblero2020@gmail.com>
2023-05-04 10:00:39 +0300

    fixed mobile version

--- src/components/shedule.js
+++ src/components/shedule.js
@@ -9,13 +9,6 @@
 
 import Swiper from 'swiper';
 if ($('.sv-body').length > 0) {
-  $('.sv-body').find('.js-navigation-item:first-child').addClass('_active');
-
-  function scheduleChange(activeIndex) {
-    $('.sv-body.is-active').find('.js-navigation-item').removeClass('_active');
-    $('.sv-body.is-active').find('.js-navigation-item').eq(activeIndex).addClass('_active');
-   
-  }
   const scheduleSwiperSettings = {
     slidesPerView: 'auto',
     spaceBetween: 10,
@@ -28,60 +21,25 @@ if ($('.sv-body').length > 0) {
   };
   const scheduleSwiper = new Swiper('.schedule-swiper', {
     ...scheduleSwiperSettings,
-    on: {
-      transitionEnd: function() {
-        var activeIndex = this.activeIndex;
-        scheduleChange(activeIndex);
-        
-      },
-      
-    },
   });
 
   const scheduleSwiper1 = new Swiper('.schedule-swiper1', {
     ...scheduleSwiperSettings,
-    on: {
-      transitionEnd: function() {
-        var activeIndex = this.realIndex;
-        scheduleChange( activeIndex);
-        
-      },
-    },
+   
   });
 
   const scheduleSwiper2 = new Swiper('.schedule-swiper2', {
     ...scheduleSwiperSettings,
-    on: {
-      slideChange: function() {
-        var activeIndex = this.activeIndex;
-        scheduleChange(activeIndex);
-      },
-    },
+    
   });
 
-  $('.js-navigation-item').on('click', function() {
-    $(this).closest('.sv-body').find('.js-navigation-item').not($(this)).removeClass('_active');
-    $(this).addClass('_active');
-    let pgItemIndex = $(this).index();
-    let dataTab = $(this).closest('.sv-body').data('tab');
-
-    if (dataTab === 1) {
-      scheduleSwiper1.slideTo(pgItemIndex, 300, false);
-    } else if (dataTab === 2) {
-      scheduleSwiper2.slideTo(pgItemIndex, 300, false);
-    } else {
-      scheduleSwiper.slideTo(pgItemIndex, 300, false);
-    }
-  });
 
   $('.js-navigation-item').on('mouseenter', function() {
-    let pgItemIndex = $(this).index() + 1;
-    $('.sv-time--col-' + pgItemIndex + '').addClass('_selected');
+    $(this).closest('.swiper-slide').find('.sv-time').addClass('_selected');
   });
 
   $('.js-navigation-item').on('mouseleave', function() {
-    let pgItemIndex = $(this).index() + 1;
-    $('.sv-time--col-' + pgItemIndex + '').removeClass('_selected');
+    $(this).closest('.swiper-slide').find('.sv-time').removeClass('_selected');
   });
 
 
