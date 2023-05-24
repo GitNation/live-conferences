571bbed799d6ccfb796d9ea533fcf7766f7ac8b4
lero62 <weblero2020@gmail.com>
2023-05-24 09:52:38 +0300

    fix tito button

--- src/app.js
+++ src/app.js
@@ -219,33 +219,35 @@ if ($('.hero__switch').length > 0) {
 }
 // tito widjet / fixed button
 if ($('.tito-block').length > 0) {
-  tito('on:widget:loaded', function() {
-    let headerHeight = 0;
-
-    if ($('.header').not($('._not-tito')).length > 0) {
-      headerHeight = $('.header').outerHeight();
-    }
-    $('.tito-submit').css({ bottom: headerHeight + 20 + 'px' });
-
-    $(window).on(' scroll resize', function() {
+  $(window).on('load', function() {
+    tito('on:widget:loaded', function() {
       let headerHeight = 0;
-      let scrollTop = $(this).scrollTop();
+
       if ($('.header').not($('._not-tito')).length > 0) {
         headerHeight = $('.header').outerHeight();
       }
-
       $('.tito-submit').css({ bottom: headerHeight + 20 + 'px' });
 
-      // $('.tito-block').each(function() {
-      let titoBlockOffsetTop = $('.tito-block:visible').offset().top;
-      let titoBlockHeight = $('.tito-block:visible').outerHeight();
+      $(window).on(' scroll resize', function() {
+        let headerHeight = 0;
+        let scrollTop = $(this).scrollTop();
+        if ($('.header').not($('._not-tito')).length > 0) {
+          headerHeight = $('.header').outerHeight();
+        }
 
-      if (scrollTop >= titoBlockOffsetTop + titoBlockHeight - window.innerHeight + headerHeight + 18) {
-        $('.tito-block:visible').addClass('_offset');
-      } else {
-        $('.tito-block:visible').removeClass('_offset');
-      }
-      // });
+        $('.tito-submit').css({ bottom: headerHeight + 20 + 'px' });
+
+        // $('.tito-block').each(function() {
+        let titoBlockOffsetTop = $('.tito-block:visible').offset().top;
+        let titoBlockHeight = $('.tito-block:visible').outerHeight();
+
+        if (scrollTop >= titoBlockOffsetTop + titoBlockHeight - window.innerHeight + headerHeight + 18) {
+          $('.tito-block:visible').addClass('_offset');
+        } else {
+          $('.tito-block:visible').removeClass('_offset');
+        }
+        // });
+      });
     });
   });
 }
