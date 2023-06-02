6e617a3dbe89f459d94b852c08fdf23a4d643f57
lero62 <weblero2020@gmail.com>
2023-06-02 17:33:00 +0300

    fix bug resize

--- src/components/shedule.js
+++ src/components/shedule.js
@@ -40,14 +40,17 @@ if ($('.sv-body').length > 0) {
     });
   });
 
-  // window.addEventListener('resize', function() {
-  //   setTimeout(function() {
-  //     $('.sv-time[data-sv-row]').each(function() {
-  //       var attr = $(this).data('sv-row');
-  //       equalheight('[data-sv-row="' + attr + '"]');
-  //     });
-  //   });
-  // });
+
+  window.addEventListener('resize', function() {
+    if (window.innerWidth < 767 || window.innerWidth >= 768) {
+      setTimeout(function() {
+        $('.sv-time[data-sv-row]').each(function() {
+          var attr = $(this).data('sv-row');
+          equalheight('[data-sv-row="' + attr + '"]');
+        });
+      });
+    }
+  });
 
 }
 
