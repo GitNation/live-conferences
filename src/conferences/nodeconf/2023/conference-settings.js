5e2db7e8713dbe4a79da68800d4f75316313e50d
lero62 <weblero2020@gmail.com>
2023-10-23 16:03:05 +0300

    add arhive 2023 nodeconf

---
+++ src/conferences/nodeconf/2023/conference-settings.js
@@ -0,0 +1,23 @@
+const conferenceTitle = 'Node_Cyber_Summit';
+const eventYear = 'Y2023';
+
+const tagColors = {
+  default: {
+    tagBG: 'black',
+    color: 'white',
+  },
+};
+
+const speakerAvatar = {
+  dimensions: {
+    avatarWidth: 500,
+    avatarHeight: 500,
+  },
+};
+
+module.exports = {
+  tagColors,
+  speakerAvatar,
+  conferenceTitle,
+  eventYear,
+};
