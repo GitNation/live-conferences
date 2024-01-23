6b83eff2caee0d00f5cd02bb62dd66a0c6291f25
lero62 <weblero2020@gmail.com>
2024-01-23 17:18:32 +0200

    add new sponsor C3 fest to all conferences

--- src/components/sponsorImagesResize.js
+++ src/components/sponsorImagesResize.js
@@ -3,13 +3,15 @@ export default function sponsorImagesResize() {
     let imagesXl = document.querySelectorAll('.sponsors-block.Platinum img');
     let imagesLg = document.querySelectorAll('.sponsors-block.Gold img');
     let imagesMd = document.querySelectorAll('.sponsors-block.Silver img');
-    let imagesSm = document.querySelectorAll('.sponsors-block.MediaPartner img, .sponsors-block.Partner img, .sponsors-block.TechPartner img');
+    let imagesSm = document.querySelectorAll(
+      '.sponsors-block.MediaPartner img, .sponsors-block.Partner img, .sponsors-block.TechPartner img, .sponsors-block.EntertainmentPartner img'
+    );
     let containerWidth = document.querySelector('.sponsors-block__list').offsetWidth;
 
     function adjustImageWidth(image, imagesInRow, multiplayer) {
-      let widthBase   = containerWidth / (imagesInRow * multiplayer);
+      let widthBase = containerWidth / (imagesInRow * multiplayer);
       let scaleFactor = 0.6; // 0.525;
-      let imageRatio  = image.naturalWidth / image.naturalHeight;
+      let imageRatio = image.naturalWidth / image.naturalHeight;
 
       image.width = Math.pow(imageRatio, scaleFactor) * widthBase;
     }
@@ -66,7 +68,6 @@ export default function sponsorImagesResize() {
         for (let image of imagesSm) {
           adjustImageWidth(image, 5, 2);
         }
-
       }
     }
 
