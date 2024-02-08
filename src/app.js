3cdd571bc8194025cb7fe5b59474f69899ebb099
lero62 <weblero2020@gmail.com>
2024-02-08 12:03:30 +0200

    refact code

--- src/app.js
+++ src/app.js
@@ -44,37 +44,6 @@ import noTouch from './components/noTouch';
 import ticketNotFound from './components/ticketNotFound';
 import { getCLS, getFID, getLCP, getFCP, getTTFB } from 'web-vitals';
 
-// const BASE_URL = 'https://ems.gitnation.org';
-const BASE_URL = 'http://localhost:3000';
-
-const popup = document.querySelector('.popup-container');
-const body = document.querySelector('body');
-
-if (popup && new Date() > new Date(confFinished)) {
-  fetch(`${BASE_URL}/api/events/promoted`)
-    .then((res) => {
-      return res.json();
-    })
-    .then((event) => {
-      popup.classList.add('is-active');
-      body.classList.add('is-no-scroll');
-
-      const startDate = new Date(event.startDate).toLocaleDateString('en-US', { month: 'long', day: 'numeric' });
-      const endDate = new Date(event.endDate).toLocaleDateString('en-US', { day: 'numeric' });
-      const endYear = new Date(event.endDate).getFullYear();
-
-      popup.style.setProperty('--accentColor', event.brand.accentCSS);
-      document.querySelector('#promo__name').innerText = event.name;
-      document.querySelector('#promo__img img').src = event.brand.icon;
-      document.querySelector('#promo__img img').alt = event.brand.name;
-      document.querySelector('#promo__title').innerText = event.brand.tagline;
-      document.querySelector('#promo__link').href = event.brand.domain;
-      document.querySelector('#promo__location').innerText = event.location;
-      document.querySelector('#promo__startDate').innerText = startDate;
-      document.querySelector('#promo__endDate').innerText = endDate + ', ' + endYear;
-    })
-    .catch((error) => console.log(error));
-}
 Sentry.init({
   dsn: 'https://60b10886207d461a8b333f66e3d86ebf@o513607.ingest.sentry.io/5615857',
   integrations: [new Integrations.BrowserTracing()],
