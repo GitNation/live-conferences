ae55d51379b8199e44f7d938856e672b4fcebf54
lero62 <weblero2020@gmail.com>
2024-02-23 10:25:48 +0200

    visible notice panel when conf finished and  delete pages outpated

--- src/components/noticePanel.js
+++ src/components/noticePanel.js
@@ -1,7 +1,14 @@
-
 const close = $('.js-notice-close');
 const panel = $('.js-notice-panel');
+
+let visibleNoticePanel = new Date(confFinished);
+visibleNoticePanel = new Date(new Date(visibleNoticePanel.setDate(visibleNoticePanel.getDate() + 1)).setHours(0, 0, 0));
 
 close.on('click', function() {
   panel.hide();
 });
+if (panel.length) {
+  if (visibleNoticePanel < new Date()) {
+    panel.show();
+  }
+}
