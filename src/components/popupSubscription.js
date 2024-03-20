64acf762ff545e4eccad924854ed8b3be9d812ff
lero62 <weblero2020@gmail.com>
2024-03-20 11:02:02 +0200

    fix script popup subscribe

--- src/components/popupSubscription.js
+++ src/components/popupSubscription.js
@@ -2,9 +2,9 @@ export default function popupSubscription() {
   const container = $('.js-subscription-container');
   const close = $('.js-subscription-close');
   const form = $('.js-subscription-form');
-  const showOnYOffset = document.body.scrollHeight / 5;
+  const showOnYOffset = document.body.scrollHeight / 7;
 
-  $(window).scroll(function() { 
+  $(window).on('scroll', function() {
     var scrollY = $(document).scrollTop();
     // console.log('showOnYOffset ' + showOnYOffset);
 
@@ -14,19 +14,17 @@ export default function popupSubscription() {
     }
   });
 
+  close.on('click', function() {
+    container.addClass('closed');
+    localStorage.setItem('popupSubscriptionIsHidden', true);
+  });
+
   if (localStorage.popupSubscriptionIsHidden !== 'true') {
     if (form.length) {
       form[0].addEventListener('submit', () => {
-        container.addClass('done');
+        container.removeClass('visible');
         localStorage.setItem('popupSubscriptionIsHidden', true);
       });
-
-      for (const closeItem of close) {
-        closeItem.addEventListener('click', () => {
-          container.addClass('closed');
-          localStorage.setItem('popupSubscriptionIsHidden', true);
-        });
-      }
     }
   }
 }
