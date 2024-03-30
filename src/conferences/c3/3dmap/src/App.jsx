cf548a3350792f68168600e47ce2f545024d6410
PixelsCommander <denis.radin@gmail.com>
2024-03-30 12:44:50 +0800

    Moves camera a bit so it is better zoomed

--- src/conferences/c3/3dmap/src/App.jsx
+++ src/conferences/c3/3dmap/src/App.jsx
@@ -1,12 +1,12 @@
 import {Canvas} from '@react-three/fiber';
 import {Experience} from './components/Experience';
 import {Environment, OrbitControls, ScrollControls} from '@react-three/drei';
-import {Suspense} from "react";
+import {Suspense} from 'react';
 
 function App() {
   return (
     // y -0.015 x + 0.01
-    <Canvas shadows camera={{position: [-2.7, 3, 4], fov: 27}}>
+    <Canvas shadows camera={{position: [-2, 3.5, 3], fov: 27}}>
       <Suspense fallback={null}>
         <Environment preset="city"/>
         <ScrollControls pages={2} damping={0.2}>
