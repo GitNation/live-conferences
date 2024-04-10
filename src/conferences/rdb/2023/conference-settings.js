4048c4e2c2ffa45f585c860d440ec1df307cab77
lero62 <weblero2020@gmail.com>
2024-04-10 15:02:25 +0300

    add archive 2023 for rdb

---
+++ src/conferences/rdb/2023/conference-settings.js
@@ -0,0 +1,175 @@
+const conferenceTitle = 'React_Day_Berlin';
+const eventYear = 'Y2023';
+
+const tagColors = {
+  deno: {
+    tagBG: '#7f00bc',
+    color: '#ffffff',
+  },
+  innovation: {
+    tagBG: '#9a8700',
+    color: '#ffffff',
+  },
+  documentation: {
+    tagBG: '#36a901',
+    color: '#ffffff',
+  },
+  'game development': {
+    tagBG: '#03B5ED',
+    color: '#ffffff',
+  },
+  'developer experience': {
+    tagBG: '#7AB464',
+    color: '#ffffff',
+  },
+  'best practices': {
+    tagBG: '#fff40d',
+    color: '#ffffff',
+  },
+  devtools: {
+    tagBG: '#230cea',
+    color: '#ffffff',
+  },
+  'react server components': {
+    tagBG: '#bc0063',
+    color: '#ffffff',
+  },
+  astro: {
+    tagBG: '#810cea',
+    color: '#ffffff',
+  },
+  'react native': {
+    tagBG: '#ed7303',
+    color: '#ffffff',
+  },
+  patterns: {
+    tagBG: '#00a27a',
+    color: '#ffffff',
+  },
+  'api development': {
+    tagBG: '#edb403',
+    color: '#ffffff',
+  },
+  typescript: {
+    tagBG: '#d00000',
+    color: '#ffffff',
+  },
+  react: {
+    tagBG: '#36CDC4',
+    color: '#ffffff',
+  },
+  'artificial intelligence': {
+    tagBG: '#287f00',
+    color: '#ffffff',
+  },
+  'case study': {
+    tagBG: '#b05454',
+    color: '#ffffff',
+  },
+  'machine learning': {
+    tagBG: '#D00AE1',
+    color: '#ffffff',
+  },
+  solidjs: {
+    tagBG: '#804aea',
+    color: '#ffffff',
+  },
+  performance: {
+    tagBG: '#004c9a',
+    color: '#ffffff',
+  },
+  'next.js': {
+    tagBG: '#007e9a',
+    color: '#ffffff',
+  },
+  graphql: {
+    tagBG: '#009a1a',
+    color: '#ffffff',
+  },
+  architecture: {
+    tagBG: '#d8bd00',
+    color: '#ffffff',
+  },
+  a11y: {
+    tagBG: '#d85900',
+    color: '#ffffff',
+  },
+  'web development': {
+    tagBG: '#ec60b4',
+    color: '#ffffff',
+  },
+  'web apps': {
+    tagBG: '#6098ec',
+    color: '#ffffff',
+  },
+  'state management': {
+    tagBG: '#2a9c97',
+    color: '#ffffff',
+  },
+  'micro-frontends': {
+    tagBG: '#ff5dc4',
+    color: '#ffffff',
+  },
+  security: {
+    tagBG: '#3cd9ff',
+    color: '#ffffff',
+  },
+  'design systems': {
+    tagBG: '#ff7e7e',
+    color: '#ffffff',
+  },
+  'web components': {
+    tagBG: '#60d39f',
+    color: '#ffffff',
+  },
+  turbopack: {
+    tagBG: '#6c9ab7',
+    color: '#ffffff',
+  },
+  'team productivity': {
+    tagBG: '#a23efb',
+    color: '#ffffff',
+  },
+  'browser api': {
+    tagBG: '#fb3ed2',
+    color: '#ffffff',
+  },
+  career: {
+    tagBG: '#3e46fb',
+    color: '#ffffff',
+  },
+  'edge functions': {
+    tagBG: '#3ee5fb',
+    color: '#ffffff',
+  },
+  css: {
+    tagBG: '#3fdc7e',
+    color: '#ffffff',
+  },
+  'developer challenges': {
+    tagBG: '#dc833f',
+    color: '#ffffff',
+  },
+
+  default: {
+    tagBG: 'black',
+    color: 'ffffff',
+  },
+};
+
+const speakerAvatar = {
+  dimensions: {
+    avatarWidth: 500,
+    avatarHeight: 500,
+  },
+};
+
+const timezone = 'Europe/Berlin';
+
+module.exports = {
+  tagColors,
+  speakerAvatar,
+  conferenceTitle,
+  eventYear,
+  timezone,
+};
