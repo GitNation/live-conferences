6f0eb84cf911fefc24c1685c7b4501c5204ed314
lero62 <weblero2020@gmail.com>
2024-04-26 12:44:19 +0300

    add animation to subtitle

---
+++ src/conferences/pc/js/animations.js
@@ -0,0 +1,16 @@
+function toggleClass(selector, className, delay) {
+  const items = document.querySelectorAll(selector);
+  let index = items.length - 1;
+
+  return items.length
+    ? setInterval(() => {
+        items[index].classList.remove(className);
+        index = -~index % items.length;
+        items[index].classList.add(className);
+      }, delay)
+    : null;
+}
+
+if (document.querySelector('.hero__subtitle')) {
+  toggleClass('.hero__subtitle > div', 'is-active', 2500);
+}
