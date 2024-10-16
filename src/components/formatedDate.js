3339493c62384989320b0a794553701564e08866
lero62 <weblero2020@gmail.com>
2024-10-16 13:10:20 +0300

    fix schedule

--- src/components/formatedDate.js
+++ src/components/formatedDate.js
@@ -1,15 +1,27 @@
-function formatDate(dateString) {
+function formatDate(dateString, timeZone = 'UTC') {
   const date = new Date(dateString);
-  const options = { month: 'long', day: 'numeric' };
+
+  if (isNaN(date)) {
+    console.error('Invalid date:', dateString);
+    return dateString;
+  }
+
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    timeZone,
+  };
+
   return date.toLocaleDateString('en-US', options);
 }
 
 if (document.querySelectorAll('.js-format-date')) {
   document.querySelectorAll('.js-format-date').forEach((element) => {
     const dateValue = element.getAttribute('data-date');
+    const timeZone = element.getAttribute('data-timezone') || 'UTC'; // Default to UTC if no timezone is provided
 
     if (dateValue) {
-      const formattedDate = formatDate(dateValue);
+      const formattedDate = formatDate(dateValue, timeZone);
       element.textContent = formattedDate;
     }
   });
