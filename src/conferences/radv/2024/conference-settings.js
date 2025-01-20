a18b3cc3e78ec560c9eb3abc95732fd007510ec0
Lero <weblero2020@gmail.com>
2025-01-20 19:25:37 +0200

    arhive radv 2024

---
+++ src/conferences/radv/2024/conference-settings.js
@@ -0,0 +1,134 @@
+const conferenceTitle = 'React_Advanced_London';
+const eventYear = 'Y2024';
+
+const tagColors = {
+  // 'AI/ML': {
+  //   tagBG: '#dd4b39',
+  //   color: '#fbff00',
+  // },
+  // 'UX/UI': {
+  //   tagBG: '#5e8de3',
+  //   color: '#FFFFFF',
+  // },
+  // NodeJS: {
+  //   tagBG: '#8cc84b',
+  //   color: '#fff',
+  // },
+  // WebGL: {
+  //   tagBG: '#ff7302',
+  //   color: '#ffffff',
+  // },
+  // Ecosystem: {
+  //   tagBG: '#fff40d',
+  //   color: '#ae4f01',
+  // },
+  // Language: {
+  //   tagBG: '#fff40d',
+  //   color: '#ae4f01',
+  // },
+  // GraphQL: {
+  //   tagBG: '#f200fa',
+  //   color: '#fff',
+  // },
+  // GQL: {
+  //   tagBG: '#198bb0',
+  //   color: '#FFFFFF',
+  // },
+  // VueJS: {
+  //   tagBG: '#3fb984',
+  //   color: '#fff',
+  // },
+  // Performance: {
+  //   tagBG: '#00ed24',
+  //   color: '#00410a',
+  // },
+  // TypeScript: {
+  //   tagBG: '#007acc',
+  //   color: '#fff',
+  // },
+  // Angular: {
+  //   tagBG: '#dd0031',
+  //   color: '#FFFFFF',
+  // },
+  // AlpineJS: {
+  //   tagBG: '#8ac1ce',
+  //   color: '#fff',
+  // },
+  // React: {
+  //   tagBG: '#61dbfb',
+  //   color: '#004d61',
+  // },
+  // I18N: {
+  //   tagBG: '#866bba',
+  //   color: '#b9efff',
+  // },
+  // Career: {
+  //   tagBG: '#9c00ff',
+  //   color: '#fff',
+  // },
+  // Testing: {
+  //   tagBG: '#ffaa45',
+  //   color: '#fff',
+  // },
+  // 'API testing': {
+  //   tagBG: '#0084ff',
+  //   color: '#fff',
+  // },
+  // Automation: {
+  //   tagBG: '#ffc000',
+  //   color: '#fff',
+  // },
+  // 'E2E testing': {
+  //   tagBG: '#ff6600',
+  //   color: '#fff',
+  // },
+  // 'Mobile testing': {
+  //   tagBG: '#8400ff',
+  //   color: '#fff',
+  // },
+  // Performance: {
+  //   tagBG: '#0054ff',
+  //   color: '#fff',
+  // },
+  // Security: {
+  //   tagBG: '#ba00ff',
+  //   color: '#fff',
+  // },
+  // TestOps: {
+  //   tagBG: '#55f200',
+  //   color: '#fff',
+  // },
+  // 'UI testing': {
+  //   tagBG: '#00ded1',
+  //   color: '#fff',
+  // },
+  // 'Unit testing': {
+  //   tagBG: '#ff00c6',
+  //   color: '#fff',
+  // },
+  // 'Visual regressions': {
+  //   tagBG: '#323232',
+  //   color: '#fff',
+  // },
+  default: {
+    tagBG: '#24fc89',
+    color: 'black',
+  },
+};
+
+const speakerAvatar = {
+  dimensions: {
+    avatarWidth: 600,
+    avatarHeight: 600,
+  },
+};
+
+const timezone = 'Europe/London';
+
+module.exports = {
+  tagColors,
+  speakerAvatar,
+  conferenceTitle,
+  eventYear,
+  timezone,
+};
