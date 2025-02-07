65f2ed95d140b067cd8bfa04d22e362ebffa00b9
Lero <weblero2020@gmail.com>
2025-02-07 12:46:20 +0200

    fix script random speaker video and enable video in hero section

--- src/components/playRandomSpeakerVideo.js
+++ src/components/playRandomSpeakerVideo.js
@@ -1,12 +1,63 @@
 export default function playRandomVideo() {
-  const videos = Array.from(document.querySelectorAll('.js-speaker-video'));
-  if (!videos.length) return;
-  const randomIndex = Math.floor(Math.random() * videos.length);
-  const videoElement = videos[randomIndex];
-  videoElement.play();
+  const speakers = document.querySelectorAll('.speaker');
+  const videos = document.querySelectorAll('.js-speaker-video');
+  let randomPlayTimeout;
+  let isHovering = false;
+  let lastPlayedIndex = -1;
+  let isPlaying = false;
 
-  videoElement.onended = () => {
+  function playRandomVideo() {
+    if (isHovering || isPlaying) return;
+
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * videos.length);
+    } while (randomIndex === lastPlayedIndex);
+
+    lastPlayedIndex = randomIndex;
+    const videoElement = videos[randomIndex];
+
+    isPlaying = true;
+    videoElement.style.display = 'block';
     videoElement.currentTime = 0;
-    setTimeout(playRandomVideo, 5000);
-  };
+    videoElement.play();
+
+    videoElement.onended = () => {
+      videoElement.style.display = 'none';
+      isPlaying = false;
+      randomPlayTimeout = setTimeout(playRandomVideo, 2000);
+    };
+  }
+
+  speakers.forEach((speaker) => {
+    const video = speaker.querySelector('.js-speaker-video');
+
+    speaker.addEventListener('mouseenter', () => {
+      isHovering = true;
+      clearTimeout(randomPlayTimeout);
+      if (video) {
+        if (isPlaying) {
+          videos.forEach((vid) => {
+            vid.pause();
+            vid.style.display = 'none';
+          });
+          isPlaying = false;
+        }
+        video.style.display = 'block';
+        video.currentTime = 0;
+        video.play();
+      }
+    });
+
+    speaker.addEventListener('mouseleave', () => {
+      isHovering = false;
+      if (video) {
+        video.pause();
+        video.style.display = 'none';
+      }
+      randomPlayTimeout = setTimeout(playRandomVideo, 2000);
+    });
+  });
+
+  randomPlayTimeout = setTimeout(playRandomVideo, 2000);
 }
