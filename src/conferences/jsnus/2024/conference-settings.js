0d4980e213f89a3282ab9c68bc8caf3a2d063ae1
Lero <weblero2020@gmail.com>
2025-02-17 15:17:52 +0200

    add 2024 jsnus and rsus

---
+++ src/conferences/jsnus/2024/conference-settings.js
@@ -0,0 +1,189 @@
+const conferenceTitle = 'JSNation_US';
+const eventYear = 'Y2024';
+
+const tagColors = {
+  // 'Vue.js': {
+  //   tagBG: '#97f989',
+  //   color: '#030303',
+  // },
+  // 'State Management': {
+  //   tagBG: '#a5d1b3',
+  //   color: '#030303',
+  // },
+  // 'Composition API': {
+  //   tagBG: '#974ca6',
+  //   color: '#fff',
+  // },
+  // Vuetify: {
+  //   tagBG: '#633d30',
+  //   color: '#fff',
+  // },
+  // Animation: {
+  //   tagBG: '#3db366',
+  //   color: '#fff',
+  // },
+  // 'Vue 3': {
+  //   tagBG: '#9fa8e1',
+  //   color: '#fff',
+  // },
+  // 'Nuxt.js': {
+  //   tagBG: '#f03c6d',
+  //   color: '#fff',
+  // },
+  // 'Performance': {
+  //   tagBG: '#7f8861',
+  //   color: '#fff',
+  // },
+  // JAMstack: {
+  //   tagBG: '#2b6abe',
+  //   color: '#fff',
+  // },
+  // Vite: {
+  //   tagBG: '#bf11af',
+  //   color: '#fff',
+  // },
+  // 'Scalable code': {
+  //   tagBG: '#255026',
+  //   color: '#fff',
+  // },
+  // 'Vue.js in the Backend': {
+  //   tagBG: '#ec7f6a',
+  //   color: '#fff',
+  // },
+  // 'Chakra UI Vue': {
+  //   tagBG: '#f2c029',
+  //   color: '#fff',
+  // },
+  // 'CI/CD': {
+  //   tagBG: '#20f53d',
+  //   color: '#030303',
+  // },
+  // 'Vuex': {
+  //   tagBG: '#fe74fe',
+  //   color: '#fff',
+  // },
+  // 'Developer Experience': {
+  //   tagBG: '#573bed',
+  //   color: '#fff',
+  // },
+  // 'Abstractions': {
+  //   tagBG: '#8a1b07',
+  //   color: '#fff',
+  // },
+  // 'VeeValidate': {
+  //   tagBG: '#3fc6f8',
+  //   color: '#fff',
+  // },
+  // 'Form Validation': {
+  //   tagBG: '#f9380a',
+  //   color: '#fff',
+  // },
+  // 'Use Case': {
+  //   tagBG: '#fbacf6',
+  //   color: '#fff',
+  // },
+  // 'Security Testing': {
+  //   tagBG: '#37fcea',
+  //   color: '#030303',
+  // },
+  // 'SEO & JS': {
+  //   tagBG: '#fe8f06',
+  //   color: '#fff',
+  // },
+  // 'UI': {
+  //   tagBG: '#6de800',
+  //   color: '#fff',
+  // },
+
+  // 'TypeScript': {
+  //   tagBG: '#ff0060',
+  //   color: '#fff',
+  // },
+  // 'Scaling': {
+  //   tagBG: '#9b43ff',
+  //   color: '#fff',
+  // },
+  // 'Recoil.js': {
+  //   tagBG: '#85b700',
+  //   color: '#fff',
+  // },
+  // 'Recoil.js': {
+  //   tagBG: '#85b700',
+  //   color: '#fff',
+  // },
+  // 'a11y': {
+  //   tagBG: '#0079b7',
+  //   color: '#fff',
+  // },
+  // 'Best Practices': {
+  //   tagBG: '#70b700',
+  //   color: '#fff',
+  // },
+  // 'Video / Media': {
+  //   tagBG: '#ffae00',
+  //   color: '#fff',
+  // },
+  // 'Web Text Editors': {
+  //   tagBG: '#ff3c00',
+  //   color: '#fff',
+  // },
+  // 'Web Workers': {
+  //   tagBG: '#ff00c0',
+  //   color: '#fff',
+  // },
+  // 'Case Study': {
+  //   tagBG: '#424698',
+  //   color: '#fff',
+  // },
+  // 'Advanced React': {
+  //   tagBG: '#819842',
+  //   color: '#fff',
+  // },
+  // 'Jamstack/Headless': {
+  //   tagBG: '#3d7072',
+  //   color: '#fff',
+  // },
+  // 'Magic': {
+  //   tagBG: '#995d3d',
+  //   color: '#fff',
+  // },
+  // 'Fiber': {
+  //   tagBG: '#993d3d',
+  //   color: '#fff',
+  // },
+  // 'React Components': {
+  //   tagBG: '#3d997c',
+  //   color: '#fff',
+  // },
+  // 'Panel Discussion': {
+  //   tagBG: '#e1bb49',
+  //   color: '#fff',
+  // },
+  default: {
+    tagBG: '#8D8D8D',
+    color: '#353535',
+  },
+};
+
+//["#cdff08", "#e9bf98", "#974ca6", "#633d30", "#3db366",
+// "#9fa8e1", "#f03c6d", "#7f8861", "#2b6abe", "#bf11af",
+// "#a5d1b3", "#255026", "#ec7f6a", "#f2c029", "#20f53d",
+// "#fe74fe", "#573bed", "#8a1b07", "#3fc6f8", "#f9380a",
+// "#fbacf6", "#37fcea", "#97f989", "#fe8f06"]
+
+const speakerAvatar = {
+  dimensions: {
+    avatarWidth: 500,
+    avatarHeight: 500,
+  },
+};
+
+const timezone = 'America/New_York';
+
+module.exports = {
+  tagColors,
+  speakerAvatar,
+  conferenceTitle,
+  eventYear,
+  timezone,
+};
