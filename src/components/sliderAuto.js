2d8eabfc5df3b0a931f0c5d9be56ebbbddaedc3d
Lero <weblero2020@gmail.com>
2025-08-21 10:07:14 +0300

    update RSUS

--- src/components/sliderAuto.js
+++ src/components/sliderAuto.js
@@ -2,12 +2,15 @@ import Swiper from 'swiper';
 export default function sliderAuto() {
   const sliderAuto = document.querySelector('.js-slider-auto');
   if (sliderAuto) {
+    const spaceBetween = parseInt(sliderAuto.dataset.spaceBetween, 10) || 20;
+
     new Swiper(sliderAuto, {
       slidesPerView: 'auto',
-      spaceBetween: 20,
+      spaceBetween,
       loop: false,
       threshold: 12,
       watchOverflow: true,
+      centerInsufficientSlides: true,
     });
   }
 }
