abe9bafbfcfd649d4310af944f7bd45d0e00a6e0
Lero <weblero2020@gmail.com>
2025-09-22 12:17:58 +0300

    add arrows to deep dives slider

--- src/components/sliderAuto.js
+++ src/components/sliderAuto.js
@@ -11,6 +11,14 @@ export default function sliderAuto() {
       threshold: 12,
       watchOverflow: true,
       centerInsufficientSlides: true,
+      navigation: {
+        nextEl: '.swiper-auto-button-next',
+        prevEl: '.swiper-auto-button-prev',
+      },
+      pagination: {
+        clickable: true,
+        el: '.swiper-auto-pagination',
+      },
     });
   }
 }
