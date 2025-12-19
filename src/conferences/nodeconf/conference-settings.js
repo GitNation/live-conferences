ded269264aa2f5a3ff26d2f4a64667175bc083ec
Lero <weblero2020@gmail.com>
2025-12-19 06:35:47 +0200

    add 2025 archive node conf

--- src/conferences/nodeconf/conference-settings.js
+++ src/conferences/nodeconf/conference-settings.js
@@ -1,26 +1,26 @@
 const conferenceTitle = 'Node_Cyber_Summit';
-const eventYear = 'Y2025';
+const eventYear = 'Y2026';
 
 const tagColors = {
-  default: {
-    tagBG: 'black',
-    color: 'white',
-  },
+	default: {
+		tagBG: 'black',
+		color: 'white',
+	},
 };
 
 const speakerAvatar = {
-  dimensions: {
-    avatarWidth: 500,
-    avatarHeight: 500,
-  },
+	dimensions: {
+		avatarWidth: 500,
+		avatarHeight: 500,
+	},
 };
 
 const timezone = 'Europe/Amsterdam';
 
 module.exports = {
-  tagColors,
-  speakerAvatar,
-  conferenceTitle,
-  eventYear,
-  timezone,
+	tagColors,
+	speakerAvatar,
+	conferenceTitle,
+	eventYear,
+	timezone,
 };
