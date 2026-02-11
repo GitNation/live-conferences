777dced5246d0666b0e0412ad8706d4e3fc1d6bf
Lero <weblero2020@gmail.com>
2026-02-11 16:48:47 +0200

    fix alert popup

--- src/components/alert-sticky.js
+++ src/components/alert-sticky.js
@@ -1,7 +1,7 @@
 const alertSticky = document.querySelector('.js-alert-sticky');
 const alertClose = document.querySelector('.js-alert-close');
 
-if (alertSticky) {
+if (alertSticky && alertClose) {
   alertClose.addEventListener('click', () => {
     alertSticky.style.display = 'none';
   });
