da9b66d106552232e2ff999354731867ead79361
Lero <weblero2020@gmail.com>
2026-02-23 13:44:27 +0200

    fix sections

--- src/components/sliderAuto.js
+++ src/components/sliderAuto.js
@@ -1,23 +1,23 @@
 import Swiper from 'swiper';
 export default function sliderAuto() {
-  const sliderAuto = document.querySelector('.js-slider-auto');
-  if (sliderAuto) {
-    const userConfig = JSON.parse(sliderAuto.dataset.config || '{}');
+  const sliders = document.querySelectorAll('.js-slider-auto');
+  sliders.forEach((slider) => {
+    const userConfig = JSON.parse(slider.dataset.config || '{}');
+    const parent = slider.parentElement;
 
-    new Swiper(sliderAuto, {
+    new Swiper(slider, {
       slidesPerView: 'auto',
       spaceBetween: 20,
       loop: false,
       threshold: 12,
       watchOverflow: true,
-      centerInsufficientSlides: true,
       navigation: {
-        nextEl: '.swiper-auto-button-next',
-        prevEl: '.swiper-auto-button-prev',
+        nextEl: parent ? parent.querySelector('.swiper-auto-button-next') : '.swiper-auto-button-next',
+        prevEl: parent ? parent.querySelector('.swiper-auto-button-prev') : '.swiper-auto-button-prev',
       },
       pagination: {
         clickable: true,
-        el: '.swiper-auto-pagination',
+        el: parent ? parent.querySelector('.swiper-auto-pagination') : '.swiper-auto-pagination',
       },
       breakpoints: {
         0: {
@@ -29,5 +29,5 @@ export default function sliderAuto() {
       },
       ...userConfig,
     });
-  }
+  });
 }
