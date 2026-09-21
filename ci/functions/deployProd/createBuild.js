1ddfaad020ab8ae500119997fdd25896bcc287e3
Lero <weblero2020@gmail.com>
2026-09-21 19:03:10 +0300

    fix: drop the extra netlify lookup from the slack deploy command

--- ci/functions/deployProd/createBuild.js
+++ ci/functions/deployProd/createBuild.js
@@ -1,15 +1,19 @@
-const { getSite, enableBuilds, startBuild, getDeploy } = require('./api');
+const { getSite, enableBuilds, startBuild } = require('./api');
 const { slackMessageOnBuild } = require('./slackMessageOnBuild');
 
 exports.createBuild = async ({ userName, commandName }) => {
   const [site] = await Promise.all([getSite(), enableBuilds(true)]);
   const build = await startBuild();
-  const deploy = await getDeploy(build.deploy_id);
+  /**
+   * Spelled out instead of read from getDeploy().links.permalink: Slack drops the
+   * command after 3 seconds, and that lookup is a whole round-trip for one string.
+   */
+  const previewUrl = `https://${build.deploy_id}--${site.name}.netlify.app`;
   const message = slackMessageOnBuild({
     userName,
     siteUrl: site.ssl_url,
     screenshotUrl: site.screenshot_url || `${site.ssl_url}/img/favicon.png`,
-    previewUrl: deploy.links.permalink,
+    previewUrl,
     commandName,
     buildId: build.id,
     deployId: build.deploy_id,
