b925f30c1a961c60c1a2498b8930aca397cfb9d0
Lero <weblero2020@gmail.com>
2026-09-21 19:07:03 +0300

    refactor: read the preview link from netlify again

--- ci/functions/deployProd/createBuild.js
+++ ci/functions/deployProd/createBuild.js
@@ -1,19 +1,15 @@
-const { getSite, enableBuilds, startBuild } = require('./api');
+const { getSite, enableBuilds, startBuild, getDeploy } = require('./api');
 const { slackMessageOnBuild } = require('./slackMessageOnBuild');
 
 exports.createBuild = async ({ userName, commandName }) => {
   const [site] = await Promise.all([getSite(), enableBuilds(true)]);
   const build = await startBuild();
-  /**
-   * Spelled out instead of read from getDeploy().links.permalink: Slack drops the
-   * command after 3 seconds, and that lookup is a whole round-trip for one string.
-   */
-  const previewUrl = `https://${build.deploy_id}--${site.name}.netlify.app`;
+  const deploy = await getDeploy(build.deploy_id);
   const message = slackMessageOnBuild({
     userName,
     siteUrl: site.ssl_url,
     screenshotUrl: site.screenshot_url || `${site.ssl_url}/img/favicon.png`,
-    previewUrl,
+    previewUrl: deploy.links.permalink,
     commandName,
     buildId: build.id,
     deployId: build.deploy_id,
